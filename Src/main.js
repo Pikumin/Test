bc06311b7dd753d3ada44adf830c4d976742ee07
Pikumin <148880034+Pikumin@users.noreply.github.com>
2023-10-24 22:52:50 +0900

    Add files via upload

--- Src/main.js
+++ Src/main.js
@@ -1,27 +1,37 @@
 async function main() {
-    await Sleep(1000)
+    //=メモ
+    //SetColor("red")
+    //DrawText( "ひよこ,",300,300)
+//     var x=47585876
+//     x=99
+// DrawText(x,10,100)
+//for=回数指定の繰り返し
+// for(var i =0;i<10;i++){
+//     DrawText(i,10,100+i*20)
+// }
+//while=繰り返し
+//var=右の文字を変数だということを宣言してる　
+// var x=0
+// while(true){
+//     SetColor("white")
+//     //DrawRect=四角形を書く
+//     DrawRect(0,0,1000,1000)//x座標，y座標
+//     x++
 
-    DrawText("Hello World!", 100, 100)
+//     //await Sleep(1000)　　//1000=1秒
 
-    await Sleep(1000)
-    
-    SetColor("red")
-    SetFont("50px serif")
-    DrawText("Let's Coding!", 100, 200)
+//     SetColor("pink")
+//     DrawText("a",x,100)
+//     await Sleep(10)//sleep=休憩時間　1000
+// }
+// //if文
 
-    await Sleep(1000)
-
-    
-    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0]
-    
-    while (true){
-        //clear
+    while(true){
         SetColor("white")
-        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1])
+        DrawRect(0,0,1000,1000)
 
-        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100)
-    
-        //cool time
-        await Sleep(1000/16)
+        SetColor("black")
+        DrawText("a", GetMouseX(), GetMouseY())
+        await Sleep(1)
     }
 }
